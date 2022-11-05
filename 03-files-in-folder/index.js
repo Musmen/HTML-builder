d9d6f0eb427e3b1e39a870f20f4c79f67ec15106
Musmen <musmen@internet.ru>
2022-11-05 15:57:15 +0300

    feat: add 'files-in-folder' task solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+
+const getFileNameAndExtension = (filePath) => {
+  const { name, ext } = path.parse(filePath);
+  return { name, extension: ext.slice(1) };
+};
+
+const logFileData = (name, extension, size) => {
+  console.log(`${name} - ${extension} - ${size}`);
+};
+
+const logFolderContent = (folderContent) => {
+  folderContent.forEach((content) => {
+    if (content.isDirectory()) return;
+
+    const innerFileFullPath = path.join(secretFolderPath, content.name);
+
+    fs.stat(innerFileFullPath, (error, fileStats) => {
+      if (error) {
+        console.log(`Error reading file ${innerFileFullPath}: `, error);
+        return;
+      }
+
+      const { name, extension } = getFileNameAndExtension(innerFileFullPath);
+      const { size } = fileStats;
+      logFileData(name, extension, size);
+    });
+  });
+};
+
+const secretFolderPath = path.join(__dirname, './secret-folder');
+const folderContentPromise = fsPromises.readdir(secretFolderPath, {
+  withFileTypes: true,
+});
+folderContentPromise.then(logFolderContent);
