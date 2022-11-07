66586a0985cac281b4293b20aa5cedcac39b6310
Musmen <musmen@internet.ru>
2022-11-07 10:15:47 +0300

    fix: fix 'write-file' task solution with ctrl+c exit

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -5,18 +5,21 @@ const outputFilePath = path.join(__dirname, './output.txt');
 const writeStream = fs.createWriteStream(outputFilePath);
 const readStream = process.stdin;
 
-console.log('Hello! Please, input your message');
-
-readStream.on('data', (input) => {
-  if (input.includes('exit')) process.exit();
-});
-
-process.addListener('exit', () => {
-  writeStream.end();
-  readStream.end();
+const onExitHandler = () => {
   console.log(
     'Your message/messages is/are in the output.txt file. Thank you!'
   );
+  writeStream.end();
+  readStream.end();
+  process.exit();
+};
+
+console.log('Hello! Please, input your message');
+
+readStream.on('data', (input) => {
+  if (input.includes('exit')) process.emit('SIGINT');
 });
+
+process.on('SIGINT', onExitHandler);
 
 readStream.pipe(writeStream);
